82d37b395e72d18cd9e3c016213f5b367f246079
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-03-22 19:39:24 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/guidiniz/workshopmongo/domain/User.java
+++ src/main/java/com/guidiniz/workshopmongo/domain/User.java
@@ -1,29 +1,68 @@
 package com.guidiniz.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Getter
-@Setter
 @Document
 public class User implements Serializable{
+	
 	private static final long serialVersionUID = 1L;
 	
-	@EqualsAndHashCode.Include
 	@Id
 	private String id;
 	private String name;
 	private String email;
 		
+	public User() {
+	}
+
+	public User(String id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
 }
