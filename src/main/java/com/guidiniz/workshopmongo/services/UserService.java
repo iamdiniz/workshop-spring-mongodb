8e7ec656f9799c31de97baceb2f7bc4b2f406e8d
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-03-23 12:08:01 -0300

    Obtendo um usuario por id & ExceptionHandler

--- src/main/java/com/guidiniz/workshopmongo/services/UserService.java
+++ src/main/java/com/guidiniz/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.guidiniz.workshopmongo.domain.User;
 import com.guidiniz.workshopmongo.repository.UserRepository;
+import com.guidiniz.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +17,9 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
