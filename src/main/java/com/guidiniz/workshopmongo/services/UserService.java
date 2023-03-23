1f63dfdb900049f31f72bcae0cb631ef87404995
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-03-23 12:23:42 -0300

    Insercao de usuario com POST

--- src/main/java/com/guidiniz/workshopmongo/services/UserService.java
+++ src/main/java/com/guidiniz/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guidiniz.workshopmongo.domain.User;
+import com.guidiniz.workshopmongo.dto.UserDTO;
 import com.guidiniz.workshopmongo.repository.UserRepository;
 import com.guidiniz.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -21,5 +22,13 @@ public class UserService {
 	
 	public User findById(String id) {
 		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
